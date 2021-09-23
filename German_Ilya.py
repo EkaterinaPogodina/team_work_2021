86358b0936a03f346c2ec3d4ebebc4f3e988c31b
ilyagerman52 <ivgerman@edu.hse.ru>
2021-09-23 21:23:27 +0300

    Ilya_German

--- German_Ilya.py
+++ German_Ilya.py
@@ -1,27 +1,21 @@
 import string
 
 
-def decorator_1(func):
+def decorator(func):
     def lower_to_upper(*args):
-        s = func(*args)
-        return s.upper()
+        return func(*args).upper()
+
     return lower_to_upper
 
 
-@decorator_1
-def my_func_for_first_task(first_str, second_str):
-    return first_str + second_str
+@decorator
+def plus_func(first, second):
+    return first + second
 
 
-def decorator_2(func):
-    def delete_vowels(*args, list_of_letters=['A', 'E', 'I', 'O', 'U', 'Y']):
-        s = func(*args)
-        for i in list_of_letters:
-            s = s.replace(i, '')
-        return s.upper()
-    return delete_vowels
-
-
-@decorator_2
-def my_func_for_second_task(str_):
-    return str_.upper()
+@decorator
+def delete_vowels(str_, list_of_letters=['A', 'E', 'I', 'O', 'U', 'Y' \
+                                         'a', 'e', 'i', 'o', 'u', 'y']):
+    for i in list_of_letters:
+        str_ = str_.replace(i, '')
+    return str_
