9f3b62097428acbfc476c50cd163704b1f71c65a
Арсений <arsenator337@gmail.com>
2021-09-24 09:21:29 +0300

    first commit

---
+++ Arsenij_Oznobikhin.py
@@ -0,0 +1,18 @@
+def CountSum(numbers):
+    return sum(numbers)
+
+
+def CountProd(numbers):
+    prod = 1
+    for number in numbers:
+        prod *= number
+    return prod
+
+
+def Valid(f, numbers):
+    for number in numbers:
+        if type(number) != int and type(number) != float:
+            print('Wrong data')
+            return None
+    return f(numbers)
+
