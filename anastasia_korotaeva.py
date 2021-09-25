de6a4df5714b3898a22820c3551022550c475626
AnastasKorotaeva <akorotaevad@gmail.com>
2021-09-25 16:21:00 +0300

    ow

---
+++ anastasia_korotaeva.py
@@ -0,0 +1,21 @@
+def turnaround(func):
+    def wrapper(el1, el2):
+        el1_new = el1[::-1]
+        el2_new = el2[::-1]
+        return func(el1_new, el2_new)
+    return wrapper
+
+
+@turnaround
+def numbers(str1, str2):
+    return int(str1) + int(str2)
+
+
+@turnaround
+def strings(str1, str2):
+    return sorted([str1, str2])
+
+
+arg1, arg2 = input().split()
+print(numbers(arg1, arg2))
+print(*strings(arg1, arg2))
