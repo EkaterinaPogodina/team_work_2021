28ac1f2c355231a9a89e6d36620c94229d049bd0
AnastasKorotaeva <91180011+AnastasKorotaeva@users.noreply.github.com>
2021-09-26 23:01:05 +0300

    Update anastasia_korotaeva.py

--- anastasia_korotaeva.py
+++ anastasia_korotaeva.py
@@ -1,4 +1,5 @@
 def turnaround(func):
+    """turns two strings around"""
     def wrapper(el1, el2):
         el1_new = el1[::-1]
         el2_new = el2[::-1]
@@ -9,11 +10,13 @@ def turnaround(func):
 
 @turnaround
 def numbers(str1, str2):
+    """two inversed numbers sum"""
     return int(str1) + int(str2)
 
 
 @turnaround
 def strings(str1, str2):
+    """two inversed strings sorted"""
     return sorted([str1, str2])
 
 
